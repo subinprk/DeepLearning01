6f72b3c646ac89ad675f9781ba2a9f8a666be530
subinprk <subpark@student.42berlin.de>
2024-10-03 19:04:12 +0900

    working but not in a Newton's method

--- set1_p7_blank.py
+++ set1_p7_blank.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# from sympy import diff
 # Read Data
 Xtest = np.loadtxt('test_features.dat')
 Ytest = np.loadtxt('test_labels.dat')
@@ -10,7 +11,6 @@ Xtrain = np.loadtxt('train_features.dat')
 Ytrain = np.loadtxt('train_labels.dat')
 Ntrain = Ytrain.shape[0]
 Xtrain = np.concatenate((np.ones((Ntrain,1)), Xtrain), axis=1)
-
 
 def sig(x):
     """
@@ -28,17 +28,31 @@ def Loss(W, X, Y):
     loss = 0
     N = Y.shape[0]
     # ---------- make your implementation here -------------
-
+    #w = np.transpose(W)
+    prediction = np.dot(X, W)
+    loss = np.sum(np.power(prediction - Y, 2)) / (2 * N)
+    loss = np.log(loss)
     # -------------------------------------------------
     return loss
-
 
 def gradL(X, Y, W):
     """
     Computes the gradient of L(W)
     """
     # ---------- make your implementation here -------------
-
+    step = 0.0001
+    grad = np.zeros_like(W)
+    for i in range(len(W)):
+        W_eps = np.copy(W)
+        W_eps[i] += step
+        grad[i] = (Loss(W_eps, X, Y) - Loss(W, X, Y)) / step
+    return grad
+    # loss = Loss(W, X, Y)
+    # print (loss)
+    # lossH = Loss(W + step, X, Y) # 0.0001 is the step size
+    # print (lossH)
+    # grad = (lossH - loss) / step
+    # return grad
     # -------------------------------------------------
 
 def hessian(X, N, W):
@@ -47,7 +61,16 @@ def hessian(X, N, W):
     """
     h_w = []
     # ---------- make your implementation here -------------
-
+    # h_w = np.zeros((3,3))
+    # for i in range(N):
+    #     elem = sig(X[i]) * (1 - sig(X[i])) * np.outer(X[i], X[i])
+    #     h_w += elem
+    # # print(h_w)
+    # return h_w
+    Z = np.dot(X, W)
+    predictions = sig(Z)
+    D = np.diag(predictions * (1 - predictions)) 
+    return np.dot(X.T, np.dot(D, X))
     # -------------------------------------------------
 
 def check_accuracy(w, X, Y):
@@ -79,9 +102,11 @@ def Newton(Xtrain, Ytrain, Xtest, Ytest, tol):
         l1_train = Loss(W, Xtrain, Ytrain)
         l1_test = Loss(W, Xtest, Ytest)
         steps += 1
+        N = Ytrain.shape[0]
         # for i in range(N):
         H = hessian(Xtrain, Ytrain.shape[0], W)
         G = gradL(Xtrain, Ytrain, W)
+        print(H)
         W = W - np.dot(np.linalg.pinv(H), G)
 
         l2_train = Loss(W, Xtrain, Ytrain)
@@ -98,6 +123,6 @@ def Newton(Xtrain, Ytrain, Xtest, Ytest, tol):
 
 W, error,steps, train_loss, test_loss = Newton(Xtrain, Ytrain, Xtest, Ytest, 10**(-8))
 print("\nNewton's\n", W, "\n Final Loss: ", error, " Steps: ", steps)
-plt.plot(range(9), train_loss, '-r^', range(9), test_loss, '-bo')
+plt.plot(range(79), train_loss, '-r^', range(79), test_loss, '-bo')
 plt.legend(['train loss', 'test loss'])
 plt.show()
